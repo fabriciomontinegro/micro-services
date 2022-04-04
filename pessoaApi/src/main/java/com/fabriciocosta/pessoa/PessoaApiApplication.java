e03584c5a66e5359cfd193a64716f0a230fbaadf
Fabricio Costa <fabriciocosta_05@hotmail.com>
2022-04-04 19:08:27 -0300

    Projeto pessoaApi criado

---
+++ pessoaApi/src/main/java/com/fabriciocosta/pessoa/PessoaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciocosta.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaApiApplication.class, args);
+	}
+
+}
